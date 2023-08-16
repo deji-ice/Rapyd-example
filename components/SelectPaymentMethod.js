0f0be665bcb2dff9d7059e9cd408089d15549574
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-16 23:36:25 +0100

    implemtnted payment method component

---
+++ components/SelectPaymentMethod.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const SelectPaymentMethod = ({ paymentType, moveToNextStep }) => {
+  const [selectedType, setSelectedType] = useState();
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+
+    setSelectedType(paymentType[e.target.value]);
+  };
+
+  useEffect(() => {
+    if (!!selectedType) {
+      console.log("selected pay", selectedType);
+    }
+  }, [selectedType]);
+
+  return (
+    <select
+      className="w-full outline-none"
+      name="type"
+      onChange={handleChange}
+      defaultValue=""
+    >
+      <option value="" disabled>
+        Please select Payment Method
+      </option>
+      {paymentType.map((payment, index) => (
+        <option className="flex" key={index} value={payment.type}>
+          <img className="w-3 h-2" src={payment.image} alt={payment.name} />{" "}
+          {payment.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+export default SelectPaymentMethod;
