29066bcbaf0824b7a241dd683a7ce465f4960842
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-16 23:35:36 +0100

    Co-authored-by: Enoch Chejieh <enochchejieh@gmail.com>

--- pages/api/payment_method.js
+++ pages/api/payment_method.js
@@ -1,13 +1,14 @@
 import { Rapyd } from "../../rapyd.js";
 
 const endpointHandler = async (req, res) => {
-  if (req.method === "GET") {
+  if (req.method === "POST") {
     try {
       const { country, currency } = req.body;
-      console.log(req.headers);
-      const response = await Rapyd.get(`/v1/payment_methods/countries/${country}?currency=${currency}`);
+      // /v1/payment_methods/countries/GB?currency=GBP'
+      const response = await Rapyd.get(
+        `/v1/payment_methods/country?country=${country}&currency=${currency}`
+      );
       const data = await response.data();
-
       return res.json(data);
     } catch (err) {
       res.json(err);
