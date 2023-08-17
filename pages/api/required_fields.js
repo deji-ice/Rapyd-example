4010990b7715afc2922c3445cae907bca3c0f62d
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-17 02:50:02 +0100

    commit

--- pages/api/required_fields.js
+++ pages/api/required_fields.js
@@ -1,11 +1,13 @@
 import { Rapyd } from "../../rapyd.js";
 
 const endpointHandler = async (req, res) => {
-  if (req.method === "GET") {
-    const { type } = req.body;
-    const response = await Rapyd.get(`/v1/payment_methods/required_fields/${type}`);
-    const data = await response.data();
+  if (req.method === "POST") {
 
+    const { type } = req.body;
+    const response = await Rapyd.get(
+      `/v1/payment_methods/required_fields/${type}`
+    );
+    const data = await response.data();
     return res.json({ data });
   }
 };
--- pages/index.js
+++ pages/index.js
@@ -4,40 +4,60 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import SelectCountry from "../components/selectCountry";
 import Loader from "../components/loader";
+import SelectPaymentMethod from "../components/SelectPaymentMethod";
+import SelectPaymentType from "../components/selectPaymentType";
 
 export default function Home() {
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("");
   const [isLoading, setLoading] = useState(false);
   const [step, setStep] = useState("select_country");
   const [paymentMethodData, setPaymentMethodData] = useState([]);
+  const [paymentTypeData, setPaymentTypeData] = useState([]);
 
   const moveToSecondStep = (country) => {
     setSelectedCountry(country);
   };
 
+  const moveToThirdStep = (paymentMethod) => {
+    setSelectedPaymentMethod(paymentMethod);
+  };
+
   const loadPaymentMethod = async () => {
     setLoading(true);
+    console.log(selectedCountry);
     try {
-      const response = await axios.get(
-      "localhost:3000/api/v1/payment_method",
-        {
-          country: selectedCountry.name,
-          currency: selectedCountry.currency_code,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            country: selectedCountry.name,
-            currency: selectedCountry.currency_code,
-          },
-        }
-      );
+      const response = await axios.post("/api/payment_method", {
+        country: selectedCountry.iso_alpha2,
+        currency: selectedCountry.currency_code,
+      });
       setPaymentMethodData(response.data);
-      setStep("payment_method");
+      console.log(response.data);
+
+      setStep("select_payment_method");
       setLoading(false);
     } catch (err) {
       console.error(err, "pay");
+    }
+  };
+
+  const loadPaymentType = async () => {
+    setLoading(true);
+    console.log(selectedPaymentMethod);
+
+    try {
+      const response = await axios.post("/api/required_fields", {
+        type: selectedPaymentMethod,
+      });
+      setPaymentTypeData(response.data.data.fields);
+      console.log(response.data.data.fields)
+      setStep("set_required_fields");
+      console.log(response.data.data.fields);
+      
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -45,12 +65,13 @@ export default function Home() {
     const getCountries = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(process.env.NEXT_PUBLIC_URL + "/countries");
+        const response = await axios.get(
+          process.env.NEXT_PUBLIC_URL + "/countries"
+        );
         setCountries(response.data);
         setLoading(false);
       } catch (err) {
         console.error(err, "country");
-
       }
     };
 
@@ -58,24 +79,76 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    if (!!selectedCountry) {
+    if (selectedCountry.name) {
       loadPaymentMethod();
     }
   }, [selectedCountry]);
+
+  useEffect(() => {
+    if (!!selectedPaymentMethod) {
+      loadPaymentType();
+      console.log(selectedPaymentMethod);
+    }
+  }, [selectedPaymentMethod]);
 
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      {isLoading ? <Loader /> : GetWorkFlow(step, countries, moveToSecondStep)}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        GetWorkFlow(
+          step,
+          countries,
+          paymentMethodData,
+          paymentTypeData,
+          moveToSecondStep,
+          moveToThirdStep
+        )
+      )}
     </Layout>
   );
 }
 
-const GetWorkFlow = (step, countries, moveToSecondStep) => {
-  if (step === "select_country") return <SelectCountry countries={countries} moveToNextStep={moveToSecondStep} />;
-  if (step === "select_payment_method") return <></>;
-  if (step === "set_required_fields") return <></>;
-  if (step === "process_payment") return <></>;
+const GetWorkFlow = (
+  step,
+  countries,
+  paymentMethodData,
+  paymentTypeData,
+  moveToSecondStep,
+  moveToThirdStep,
+) => {
+  switch (step) {
+    case "select_country":
+      return (
+        <SelectCountry
+          countries={countries}
+          moveToNextStep={moveToSecondStep}
+        />
+      );
+    case "select_payment_method":
+      return (
+        <>
+          <SelectPaymentMethod
+            moveToNextStep={moveToThirdStep}
+            paymentMethod={paymentMethodData}
+          />
+        </>
+      );
+    case "set_required_fields":
+      return (
+        <>
+          <SelectPaymentType
+            moveToNextStep={moveToThirdStep}
+            paymentType={paymentTypeData}
+          />
+        </>
+      );
+    case "process_payment":
+      return <></>;
+    default:
+      return null; // Handle unknown step case
+  }
 };
